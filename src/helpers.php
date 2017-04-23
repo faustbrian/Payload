e73f625e1a13bf1f11bc9277d262f7c47ca7c0bb
Brian Faust <faustbrian@users.noreply.github.com>
2017-04-23 14:07:42 +0300

    Apply fixes from StyleCI (#9)
    [ci skip] [skip ci]

--- src/helpers.php
+++ src/helpers.php
@@ -1,6 +1,13 @@
 <?php
 
-
+/*
+ * This file is part of Payload.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 use BrianFaust\Payload\Arr;
 use BrianFaust\Payload\Csv;
